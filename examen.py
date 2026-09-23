6e2966fb586ba3a54ecec44bd02a13964daeb242
Sebastian-Vettel05 <fsineiro@escuelalosandes.edu.ar>
2026-09-23 15:54:07 -0300

    etapa 2 y 3

--- examen.py
+++ examen.py
@@ -12,7 +12,7 @@
 # ETAPA 1 - INICIO
 # =========================
 nombre = input("¿cual es su nombre?")
-saldo = input ("¿Cual es su saldo actual?")
+saldo = int(input("¿Cual es su saldo actual?"))
 productos = ["agua", "alfajor", "tostado"]
 precios = ["$700", "$900","$2200"]
 
@@ -29,7 +29,28 @@ print("2. Alfajor    - $900")
 print("3. Tostado    - $2200")
 print("4. Consultar pedido")
 print("5. Finalizar compra")
+opcion = int(input("Elija la opcion"))
 
+if opcion == 1:
+    print("Producto: ", productos [0])
+    print("Precio: ", precios [0])
+    if saldo < 700:
+        print("No le alcanza")
+    saldo = saldo - 700
+    cant_aguas += 1
+if opcion == 2:
+    print("Producto: ", productos [1])
+    print("Precio: ", precios [1])
+    if saldo < 900:
+     print("No le alcanza")
+    saldo = saldo - 900
+    cant_alfajores += 1
+if opcion == 3:
+    print("Producto: ", productos [-1])
+    print("Precio: ", precios [-1])
+    if saldo < 2200:
+     print ("No le alcanza")
+    saldo = saldo - 2200
 # =========================
 # ETAPA 3 - CICLO PRINCIPAL
 # =========================
